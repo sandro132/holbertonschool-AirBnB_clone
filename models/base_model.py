04d19d5a91d6ed768449dfc81c42b1ec79414d7b
Bluebee45 <Santi72143@gmail.com>
2023-07-12 15:44:19 -0500

    :bug: try new code

--- models/base_model.py
+++ models/base_model.py
@@ -6,35 +6,28 @@ from datetime import datetime
 '''BaseModel'''
 
 class BaseModel:
-    def __init__(self, *args, **kwarg):
+    def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
         
         '''if len(kwargs) != 0:
-            for x, y in kwargs.items():
-                if x == "created_at" or x == "updated_at":
-                    self.__dict__[x] = datetime.strptime(y, x)
+            protected_attrs = ["created_at", "updated_at"]
+            for k, v in kwargs.items():
+                if k in protected_attrs:
+                    setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
                 else:
-                    self.__dict__[x] = y
-        else:
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.update_at = datetime.now()'''
+                    setattr(self, k, v)'''
         
     def __str__(self):
         return "[{}] ({}) ({})".format(self.__class__.__name__, self.id, self.__dict__)
     
     def save(self):
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
     
     def to_dict(self):
-        """
-        Returns a dictionary containing all keys/values
-        of __dict__ of the instance
-        """
         my_dictionary = self.__dict__.copy()
         my_dictionary["__class__"] = self.__class__.__name__
         my_dictionary["created_at"] = self.created_at.isoformat()
-        my_dictionary["update_at"] = self.update_at.isoformat()
-        return (my_dictionary)
+        my_dictionary["updated_at"] = self.updated_at.isoformat()
+        return my_dictionary
