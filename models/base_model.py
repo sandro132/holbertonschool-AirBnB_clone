7051c40e65e14258befded3c7213477790099534
Bluebee45 <124604790+Bluebee45@users.noreply.github.com>
2023-07-12 17:55:53 -0500

    Update base_model.py
    Modified file and update

--- models/base_model.py
+++ models/base_model.py
@@ -2,11 +2,13 @@
 
 import uuid
 from datetime import datetime
+import models
 
 '''BaseModel'''
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
+        '''Initialize the class'''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -14,10 +16,10 @@ class BaseModel:
         if len(kwargs) != 0:
             protected_attrs = ["created_at", "updated_at"]
             for k, v in kwargs.items():
-                if k in protected_attrs:
+                if k == "created_at" or x == "updated_at":
                     setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
                 else:
-                    setattr(self, k, v)
+                    models.storage.new(self)
         
     def __str__(self):
         return "[{}] ({}) ({})".format(self.__class__.__name__, self.id, self.__dict__)
