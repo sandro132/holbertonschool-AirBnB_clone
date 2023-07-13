03b10678240bbf0f7d20d6e1a077bec379cd8b3a
Bluebee45 <santi72143@gmail.com>
2023-07-13 00:21:26 -0500

    add some of imports to the file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,12 +6,6 @@
 """ Class FileStorage """
 
 from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 import json
 
 classtype = {
