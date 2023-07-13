f91c8ea09d7d78bf951496c51e1ae0b063ff1f5a
sandro132 <mianveo@hotmail.com>
2023-07-13 00:59:29 -0500

    :memo: szs

--- models/base_model.py
+++ models/base_model.py
@@ -1,36 +1,30 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
-
 
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
+'''BaseModel'''
 
 class BaseModel:
-    """A base class for all hbnb models"""
-    def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        self.updated_at = datetime.now()
-        storage.save()
-        storage.new(self)
-
     def __init__(self, *args, **kwargs):
-        if kwargs:
-            kwargs.pop('__class__', None)
-            for key, value in kwargs.items():
-                if key in ['created_at', 'updated_at']:
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-        storage.new(self)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        if len(kwargs) != 0:
+            protected_attrs = ["created_at", "update_at"]
+            for k, v in kwargs.items():
+                if k in protected_attrs:
+                    setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, k, v)
         
     def __str__(self):
         return "[{}] ({}) ({})".format(self.__class__.__name__, self.id, self.__dict__)
     
+    def save(self):
+        self.updated_at = datetime.now()
     
     def to_dict(self):
         my_dictionary = self.__dict__.copy()
