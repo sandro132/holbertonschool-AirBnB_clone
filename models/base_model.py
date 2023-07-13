e4c86183fb9e265ae5d3f9753c5cfaa04ecebb0b
sandro132 <mianveo@hotmail.com>
2023-07-13 13:10:35 -0500

    :memo:

--- models/base_model.py
+++ models/base_model.py
@@ -2,24 +2,26 @@
 
 import uuid
 from datetime import datetime
-import models
+from models import storage
 
 '''BaseModel'''
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-        if len(kwargs) != 0:
-            protected_attrs = ["created_at", "update_at"]
+        if kwargs:
+            protected_attrs = ["created_at", "updated_at"]
+            kwargs.pop('__class__', None)
             for k, v in kwargs.items():
                 if k in protected_attrs:
                     setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
                 else:
                     setattr(self, k, v)
-        
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        storage.new(self)
+
     def __str__(self):
         return "[{}] ({}) ({})".format(self.__class__.__name__, self.id, self.__dict__)
     
