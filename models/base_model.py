6cb18c8e41a2f104b6b8e8faf74c1bdcca4191e6
sandro132 <mianveo@hotmail.com>
2023-07-13 20:27:32 -0500

    :tada: create file_store,py and update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 
 import uuid
 from datetime import datetime
-#from models import storage
+import models
 
 '''BaseModel'''
 
@@ -10,25 +10,35 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             protected_attrs = ["created_at", "updated_at"]
+            #Function to pop tha value of the class
             kwargs.pop('__class__', None)
             for k, v in kwargs.items():
                 if k in protected_attrs:
+                    #Function to traslade the format in value
                     setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
                 else:
+                    #Function to adding any property to the  object
                     setattr(self, k, v)
+            models.storage.new(self)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
+            models.storage.new(self)
 
     def __str__(self):
+        # very important, this is the output predeterminate
         return "[{}] ({}) ({})".format(self.__class__.__name__, self.id, self.__dict__)
     
     def save(self):
+        #This methot is for save the datatime
         self.updated_at = datetime.now()
-    
+        models.storage.save()
+
     def to_dict(self):
+        #a copy of the dictionary is created so as not to modify the original 
+        # one and a value is assigned to the keys which are the instances created
+        #and return the dictionary with the new va
         my_dictionary = self.__dict__.copy()
         my_dictionary["__class__"] = self.__class__.__name__
         my_dictionary["created_at"] = self.created_at.isoformat()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+
+from models.base_model import BaseModel
+import json
+#import
+
+
+class FileStorage:
+    '''CLass File storage'''
+    def __init__(self):
+        #a private class instance is created 
+        # that fetches the json file we created
+        self.__file_path = "file.json"
+        #a empty dict is created in  a private class instance
+        self.__objects = dict()
+
+    def all(self):
+        '''Return the dictionary'''
+        return self.__objects
+
+    def new(self, obj):
+        '''The obj paramether is the base model'''
+        # get propertys
+        name_clase = type(obj).__name__
+        id_obj = obj.id
+        self.__objects[f"{name_clase}.{id_obj}"] = obj
+        
+        
+    def save(self):
+        '''save the file in the  __file__path'''
+        #create a new empty dictionary
+        new_dic = dict()
+        #iteration the __objects items
+        for k, v in self.__objects.items():
+            #get the copy of the original dictionary
+            obj_dict = v.to_dict()
+            #adds the copy to new dict
+            new_dic[k] = obj_dict
+        #serialization the objects that is BaseModel
+        serialized_obj = json.dumps(new_dic)
+        with open(self.__file_path, 'w', encoding="utf-8") as file:
+            file.write(serialized_obj)
+            
+    def reload(self):
+        '''load json and reload Model'''
+        try:
+            #open the file
+            with open(self.__file_path, encoding="utf-8") as file:
+                dict_data = json.loads(file.read())
+                #iteration the items of the dict_data
+                for k, v in dict_data.items():
+                    #search the name of the class
+                    class_name = v["__class__"]
+                    self.__objects[k] = globals()[class_name](**v)
+        except FileNotFoundError:
+            pass
