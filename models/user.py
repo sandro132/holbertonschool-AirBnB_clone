233becac4e6fd48b3cd1ea0a04f469181e820f75
sandro132 <mianveo@hotmail.com>
2023-07-15 12:04:43 -0500

    :rewind: create a new class and update base model, console and file storage

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Import"""
+
+
+from models.base_model import BaseModel
+
+
+""""""
+class User(BaseModel):
+    """create a class"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
