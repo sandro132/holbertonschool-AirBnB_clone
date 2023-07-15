9b73d31c6a2c4492e0ac89ff7cd6583432c7ee31
Bluebee45 <santi72143@gmail.com>
2023-07-15 13:55:34 -0500

    add test for amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,27 +1,52 @@
+#!/usr/bin/python3
+"""
+Unittest for Amenity
+"""
+
 import unittest
 from models.amenity import Amenity
 
 
-class AmenityTest(unittest.TestCase):
+class test_Amenity(unittest.TestCase):
+    """
+    Class test amenity
+    """
     def test_instance(self):
+        """
+        Check instance of Amenity
+        """
         self.assertIsInstance(Amenity(), Amenity)
 
-    def test_name_attribute(self):
-        self.assertTrue(hasattr(Amenity, "name"))
+    def test_name(self):
+        """
+        Test name of amenity
+        """
+        self.assertTrue(hasattr(Amenity, "name"), True)
 
-    def test_created_at_attribute(self):
-        self.assertTrue(hasattr(Amenity(), "created_at"))
+    def test_createdat(self):
+        """
+        Check created at
+        """
+        self.assertTrue(hasattr(Amenity(), "created_at"), True)
 
-    def test_updated_at_attribute(self):
-        self.assertTrue(hasattr(Amenity(), "updated_at"))
+    def test_updatedat(self):
+        """
+        Check update at
+        """
+        self.assertTrue(hasattr(Amenity(), "updated_at"), True)
 
-    def test_id_attribute(self):
-        self.assertTrue(hasattr(Amenity(), "id"))
+    def test_id(self):
+        """
+        Test for id
+        """
+        self.assertTrue(hasattr(Amenity(), "id"), True)
 
-    def test_to_dict_method(self):
-        self.assertTrue(hasattr(Amenity(), "to_dict"))
+    def test_to_dict(self):
+        """
+        Check dict
+        """
+        self.assertTrue(hasattr(Amenity(), "to_dict"), True)
 
 
 if __name__ == "__main__":
     unittest.main()
-
